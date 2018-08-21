75d062dde3db68edfcdeee106e1bf0bfc5ee0822
unknown <Brad-Hill@hotmail.com.au>
2018-08-21 20:28:17 +1000

    Added Handlebar.js to the index and main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+  findByName: function() {
+      var self = this;
+      this.store.findByName($('.search-key').val(), function(employees) {
+          $('.employee-list').html(self.employeeLiTpl(employees));
+      });
+  },
 
 showAlert: function(message,title) {
       if(navigator.notification){
@@ -24,10 +18,17 @@ showAlert: function(message,title) {
 initialize: function() {
         var self = this;
         this.store = new MemoryStore( function(){
-          self.showAlert('Store Initialized','info');
+          self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    },
+
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
 };
 
 app.initialize();
